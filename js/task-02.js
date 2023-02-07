9229e84647bfdd8fbb41481d03d83c8372cddef9
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-02-07 19:52:21 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const elList = document.querySelector("#ingredients");
+
+// const listOfItems = ingredients.map((ingredient) => {
+//   const elTextLine = document.createElement("li");
+//   elTextLine.textContent = ingredient;
+
+//   return elTextLine;
+// });
+
+const makeListOfItems = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const elTextLine = document.createElement("li");
+    elTextLine.textContent = ingredient;
+
+    return elTextLine;
+  });
+};
+
+const listOfItems = makeListOfItems(ingredients);
+
+elList.append(...listOfItems);
